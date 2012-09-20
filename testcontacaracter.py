54ccea0cb91c98c35f96dc1f294cd1da52e4d7b7
Dayane Andrade <dayandrade.info@yahoo.com.br>
2012-09-19 21:23:54 -0300

    acrescentando mais algumas linhas de teste

--- testcontacaracter.py
+++ testcontacaracter.py
@@ -4,4 +4,10 @@ from contacaracter import contaCaracter
 class TestContaCaracter(unittest.TestCase):
 
 	def test_deveria_retornar_quantidade_de_caracteres_correto(self):
-		self.assertEqual(contaCaracter(lala),{'a': 2, 'l': 2})
+		self.assertEqual(contaCaracter('lala'),{'a': 2, 'l': 2})
+		self.assertEqual(contaCaracter('lele'),{'e': 2, 'l': 2})
+		self.assertEqual(contaCaracter('lili'),{'i': 2, 'l': 2})
+		self.assertEqual(contaCaracter('lolo'),{'l': 2, 'o': 2})
+		self.assertEqual(contaCaracter('ll'),{'l': 2})
+
+unittest.main()
